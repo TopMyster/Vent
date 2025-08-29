a5b0fe2177d401e742ea08ba8d38b6c6101f69e6
TopMyster <topmyster@outlook.com>
2025-08-28 22:20:37 -0500

    playing with functions

--- src/Chatbot.jsx
+++ src/Chatbot.jsx
@@ -1,8 +1,16 @@
 import { useState } from 'react'
 export default function Chatbot() {
+    const [ messages, Setmessages ] = useState('')
+    function msg() {
+        Setmessages('hi')
+    }
     return (
         <>
+        <div>
+            {messages}
+        </div>
         <h1>Hi</h1>
+        <button onClick={msg}>hi</button>
         </>
     )
 }
